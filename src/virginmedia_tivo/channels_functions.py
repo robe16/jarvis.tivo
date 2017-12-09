0f61efb4dfc04e93d1a63e69b6c6278a9923f934
Rob <robe16@hotmail.co.uk>
2017-12-09 23:39:53 +0000

    Logging entries added for expcetions on channel_functions.py

--- src/virginmedia_tivo/channels_functions.py
+++ src/virginmedia_tivo/channels_functions.py
@@ -1,4 +1,5 @@
 from config.config import get_cfg_details_package
+from log.log import log_internal
 
 # Issue with IDE and production running of script - resolved with try/except below
 try:
@@ -10,49 +11,67 @@ except:
 
 
 def get_channel_name_from_key(key):
-    for chan in channels:
-        if channels[chan]['sd']:
-            if channels[chan]['sd']['key'] == key:
-                return chan
-        elif channels[chan]['hd']:
-            if channels[chan]['hd']['key'] == key:
-                return chan
     #
-    return False
+    try:
+        k = int(key)
+        #
+        for chan in channels:
+            if channels[chan]['sd']:
+                if channels[chan]['sd']['key'] == k:
+                    return chan
+            elif channels[chan]['hd']:
+                if channels[chan]['hd']['key'] == k:
+                    return chan
+        #
+        return False
+    except Exception as e:
+        log_internal(False, 'Attempted get channel name from key - \'{key}\''.format(key=key), desc='fail', exception=e)
+        return False
 
 
 def get_channel_details_from_key(key):
-    for chan in channels:
-        if channels[chan]['sd']:
-            if channels[chan]['sd']['key'] == key:
-                return {'name': chan, 'quality': 'sd'}
-        elif channels[chan]['hd']:
-            if channels[chan]['hd']['key'] == key:
-                return {'name': chan, 'quality': 'hd'}
     #
-    return False
+    try:
+        for chan in channels:
+            if channels[chan]['sd']:
+                if channels[chan]['sd']['key'] == key:
+                    return {'name': chan, 'quality': 'sd'}
+            elif channels[chan]['hd']:
+                if channels[chan]['hd']['key'] == key:
+                    return {'name': chan, 'quality': 'hd'}
+        #
+        return False
+    except Exception as e:
+        log_internal(False, 'Attempted get channel details from key - \'{key}\''.format(key=key), desc='fail', exception=e)
+        return False
 
 
 def get_channel_key_from_name(name):
     #
-    if name in channels.keys():
+    try:
+        if name in channels.keys():
+            #
+            package = get_cfg_details_package()
+            #
+            # Do HD first as preference
+            if channels[name]['hd']:
+                if check_package(name, 'hd', package):
+                    return channels[name]['hd']['key']
+            #
+            # If HD does not yield results, fall back to SD
+            if channels[name]['sd']:
+                if check_package(name, 'sd', package):
+                    return channels[name]['sd']['key']
         #
-        package = get_cfg_details_package()
-        #
-        # Do HD first as preference
-        if channels[name]['hd']:
-            if check_package(name, 'hd', package):
-                return channels[name]['hd']['key']
-        #
-        # If HD does not yield results, fall back to SD
-        if channels[name]['sd']:
-            if check_package(name, 'sd', package):
-                return channels[name]['sd']['key']
-    #
-    return False
+        return False
+    except Exception as e:
+        log_internal(False, 'Attempted get channel key from name - \'{name}\''.format(name=name), desc='fail', exception=e)
+        return False
 
 
 def get_channels(package):
+    #
+    try:
         #
         chans = []
         #
@@ -65,6 +84,9 @@ def get_channels(package):
             chans.append(c)
         #
         return {'channels': chans}
+    except Exception as e:
+        log_internal(False, 'Attempted get channel list from package - \'{package}\''.format(package=package), desc='fail', exception=e)
+        return False
 
 
 def check_package(name, quality, package):
