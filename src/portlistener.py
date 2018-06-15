97d969d01c3a5824776b10bd9c956a66b444307e
Rob <robe16@hotmail.co.uk>
2018-06-15 20:28:58 +0100

    Updated to new code structure that has each API script in own module and and common request-based code in common_functions.

--- src/portlistener.py
+++ src/portlistener.py
@@ -1,17 +1,22 @@
 import threading
-from bottle import HTTPError
-from bottle import get, post
-from bottle import request, run, HTTPResponse
 
-from common_functions.query_to_string import convert_query_to_string
-from config.config import get_cfg_serviceid, get_cfg_name_long, get_cfg_name_short, get_cfg_groups, get_cfg_subservices
+from bottle import get, post
+from bottle import request, run
+
 from config.config import get_cfg_port_listener
 from resources.lang.enGB.logs import *
-from resources.global_resources.variables import *
-from resources.global_resources.log_vars import logPass, logFail, logException
-from validation.validation import validate_command, validate_channel
+from resources.global_resources.log_vars import logPass
 from service.virginmedia_tivo import Virginmedia_tivo
-from log.log import log_inbound, log_internal
+from log.log import log_internal
+
+from apis.uri_config import get_config
+from apis.uri_get_commands import get_commands
+from apis.uri_post_command import post_command
+from apis.uri_get_channel import get_channel
+from apis.uri_get_channels import get_channels
+from apis.uri_post_channel import post_channel
+from apis.uri_get_recordings import get_recordings
+from apis.uri_post_enterpin import post_enterpin
 
 
 def start_bottle(port_threads):
@@ -20,392 +25,45 @@ def start_bottle(port_threads):
     # Create device
     ################################################################################################
 
-    _device = Virginmedia_tivo()
+    _virginmedia_tivo = Virginmedia_tivo()
 
     log_internal(logPass, logDescDeviceObjectCreation, description='success')
 
     ################################################################################################
-    # Enable cross domain scripting
+    # APIs
     ################################################################################################
 
-    def enable_cors(response):
-        response.headers['Access-Control-Allow-Origin'] = '*'
-        response.headers['Access-Control-Allow-Methods'] = 'GET'
-        response.headers['Access-Control-Allow-Headers'] = service_header_clientid_label
-        return response
+    @get('/config')
+    def api_get_config():
+        return get_config(request)
 
-    ################################################################################################
-    # Log arguments
-    ################################################################################################
+    @get('/commands')
+    def api_get_commands():
+        return get_commands(request, _virginmedia_tivo)
 
-    def _get_log_args(request):
-        #
-        urlparts = request.urlparts
-        #
-        try:
-            client_ip = request.headers['X-Forwarded-For']
-        except:
-            client_ip = request['REMOTE_ADDR']
-        #
-        try:
-            server_ip = request.headers['X-Real-IP']
-        except:
-            server_ip = urlparts.hostname
-        #
-        try:
-            client_user = request.headers[service_header_clientid_label]
-        except:
-            client_user = request['REMOTE_ADDR']
-        #
-        server_request_query = convert_query_to_string(request.query) if request.query_string else '-'
-        server_request_body = request.body.read() if request.body.read()!='' else '-'
-        #
-        return {'client_ip': client_ip,
-                'client_user': client_user,
-                'server_ip': server_ip,
-                'server_thread_port': urlparts.port,
-                'server_method': request.method,
-                'server_request_uri': urlparts.path,
-                'server_request_query': server_request_query,
-                'server_request_body': server_request_body}
+    @post('/command')
+    def api_post_command():
+        return post_command(request, _virginmedia_tivo)
 
-    ################################################################################################
-    # Service info & Groups
-    ################################################################################################
+    @get('/channel')
+    def api_get_channel():
+        return get_channel(request, _virginmedia_tivo)
 
-    @get(uri_config)
-    def get_config():
-        #
-        args = _get_log_args(request)
-        #
-        try:
-            #
-            data = {'service_id': get_cfg_serviceid(),
-                    'name_long': get_cfg_name_long(),
-                    'name_short': get_cfg_name_short(),
-                    'subservices': get_cfg_subservices(),
-                    'groups': get_cfg_groups()}
-            #
-            status = httpStatusSuccess
-            #
-            args['result'] = logPass
-            args['http_response_code'] = status
-            args['description'] = '-'
-            log_inbound(**args)
-            #
-            return HTTPResponse(body=data, status=status)
-            #
-        except Exception as e:
-            status = httpStatusServererror
-            #
-            args['result'] = logException
-            args['http_response_code'] = status
-            args['description'] = '-'
-            args['exception'] = e
-            log_inbound(**args)
-            #
-            raise HTTPError(status)
+    @get('/channels')
+    def api_get_channels():
+        return get_channels(request, _virginmedia_tivo)
 
-    ################################################################################################
-    # Recordings
-    ################################################################################################
+    @post('/channel')
+    def api_post_channel():
+        return post_channel(request, _virginmedia_tivo)
 
-    @get(uri_recordings)
-    def get_recordings():
-        #
-        args = _get_log_args(request)
-        #
-        try:
-            #
-            r = _device.getRecordings()
-            #
-            if not bool(r):
-                status = httpStatusFailure
-            else:
-                status = httpStatusSuccess
-            #
-            args['result'] = logPass
-            args['http_response_code'] = status
-            args['description'] = '-'
-            log_inbound(**args)
-            #
-            response = HTTPResponse()
-            response.status = status
-            enable_cors(response)
-            #
-            if not isinstance(r, bool):
-                response.body = r
-            #
-            return response
-            #
-        except Exception as e:
-            status = httpStatusServererror
-            #
-            args['result'] = logException
-            args['http_response_code'] = status
-            args['description'] = '-'
-            args['exception'] = e
-            log_inbound(**args)
-            #
-            raise HTTPError(status)
+    @get('/recordings')
+    def api_get_recordings():
+        return get_recordings(request, _virginmedia_tivo)
 
-    ################################################################################################
-    # Get channel
-    ################################################################################################
-
-    @get(uri_channel)
-    def get_channel():
-        #
-        args = _get_log_args(request)
-        #
-        try:
-            #
-            r = _device.getChan()
-            #
-            if not bool(r):
-                status = httpStatusFailure
-            else:
-                status = httpStatusSuccess
-            #
-            args['result'] = logPass
-            args['http_response_code'] = status
-            args['description'] = '-'
-            log_inbound(**args)
-            #
-            response = HTTPResponse()
-            response.status = status
-            enable_cors(response)
-            #
-            if not isinstance(r, bool):
-                response.body = r
-            #
-            return response
-            #
-        except Exception as e:
-            status = httpStatusServererror
-            #
-            args['result'] = logException
-            args['http_response_code'] = status
-            args['description'] = '-'
-            args['exception'] = e
-            log_inbound(**args)
-            #
-            raise HTTPError(status)
-
-    ################################################################################################
-    # Get channel
-    ################################################################################################
-
-    @get(uri_channels)
-    def get_channels():
-        #
-        args = _get_log_args(request)
-        #
-        try:
-            #
-            r = _device.getChannels()
-            #
-            if not bool(r):
-                status = httpStatusFailure
-            else:
-                status = httpStatusSuccess
-            #
-            args['result'] = logPass
-            args['http_response_code'] = status
-            args['description'] = '-'
-            log_inbound(**args)
-            #
-            response = HTTPResponse()
-            response.status = status
-            enable_cors(response)
-            #
-            if not isinstance(r, bool):
-                response.body = r
-            #
-            return response
-            #
-        except Exception as e:
-            status = httpStatusServererror
-            #
-            args['result'] = logException
-            args['http_response_code'] = status
-            args['description'] = '-'
-            args['exception'] = e
-            log_inbound(**args)
-            #
-            raise HTTPError(status)
-
-    ################################################################################################
-    # Post channel
-    ################################################################################################
-
-    @post(uri_channel)
-    def post_channel():
-        #
-        args = _get_log_args(request)
-        #
-        try:
-            #
-            data_dict = request.json
-            #
-            if validate_channel(data_dict):
-                #
-                channel = data_dict['channel']
-                r = _device.sendChannel(channel)
-                #
-                if not bool(r):
-                    status = httpStatusFailure
-                else:
-                    status = httpStatusSuccess
-            else:
-                status = httpStatusBadrequest
-            #
-            args['result'] = logPass
-            args['http_response_code'] = status
-            args['description'] = '-'
-            log_inbound(**args)
-            #
-            response = HTTPResponse()
-            response.status = status
-            enable_cors(response)
-            #
-            return response
-            #
-        except Exception as e:
-            status = httpStatusServererror
-            #
-            args['result'] = logException
-            args['http_response_code'] = status
-            args['description'] = '-'
-            args['exception'] = e
-            log_inbound(**args)
-            #
-            raise HTTPError(status)
-
-    ################################################################################################
-    # Get commands
-    ################################################################################################
-
-    @get(uri_commands)
-    def get_commands():
-        #
-        args = _get_log_args(request)
-        #
-        try:
-            #
-            data = _device.getCommands()
-            #
-            status = httpStatusSuccess
-            #
-            args['result'] = logPass
-            args['http_response_code'] = status
-            args['description'] = '-'
-            log_inbound(**args)
-            #
-            response = HTTPResponse()
-            response.status = status
-            response.body = data
-            enable_cors(response)
-            #
-            return response
-            #
-        except Exception as e:
-            status = httpStatusServererror
-            #
-            args['result'] = logException
-            args['http_response_code'] = status
-            args['description'] = '-'
-            args['exception'] = e
-            log_inbound(**args)
-            #
-            raise HTTPError(status)
-
-    ################################################################################################
-    # Post commands
-    ################################################################################################
-
-    @post(uri_command)
-    def post_command():
-        #
-        args = _get_log_args(request)
-        #
-        try:
-            #
-            data_dict = request.json
-            #
-            if validate_command(data_dict):
-                #
-                command = data_dict['command']
-                r = _device.sendCmd(command)
-                #
-                if not bool(r):
-                    status = httpStatusFailure
-                else:
-                    status = httpStatusSuccess
-            else:
-                status = httpStatusBadrequest
-            #
-            args['result'] = logPass
-            args['http_response_code'] = status
-            args['description'] = '-'
-            log_inbound(**args)
-            #
-            response = HTTPResponse()
-            response.status = status
-            enable_cors(response)
-            #
-            return response
-            #
-        except Exception as e:
-            status = httpStatusServererror
-            #
-            args['result'] = logException
-            args['http_response_code'] = status
-            args['description'] = '-'
-            args['exception'] = e
-            log_inbound(**args)
-            #
-            raise HTTPError(status)
-
-    ################################################################################################
-    # Pin
-    ################################################################################################
-
-    @post(uri_enterpin)
-    def post_enterpin():
-        #
-        args = _get_log_args(request)
-        #
-        try:
-            #
-            r = _device.sendPin()
-            #
-            if not bool(r):
-                status = httpStatusFailure
-            else:
-                status = httpStatusSuccess
-            #
-            args['result'] = logPass
-            args['http_response_code'] = status
-            args['description'] = '-'
-            log_inbound(**args)
-            #
-            response = HTTPResponse()
-            response.status = status
-            enable_cors(response)
-            #
-            return response
-            #
-        except Exception as e:
-            status = httpStatusServererror
-            #
-            args['result'] = logException
-            args['http_response_code'] = status
-            args['description'] = '-'
-            args['exception'] = e
-            log_inbound(**args)
-            #
-            raise HTTPError(status)
+    @post('/enterpin')
+    def api_post_enterpin():
+        return post_enterpin(request, _virginmedia_tivo)
 
     ################################################################################################
 
--- src/resources/global_resources/variables.py
+++ src/resources/global_resources/variables.py
@@ -1,14 +1,6 @@
 serviceName = 'Jarvis: TiVo'
 serviceType = 'virginmedia_tivo'
 
-uri_config = '/config'
-uri_commands = '/commands'
-uri_command = '/command'
-uri_channel = '/channel'
-uri_channels = '/channels'
-uri_recordings = '/recordings'
-uri_enterpin = '/enterpin'
-
 service_header_clientid_label = 'jarvis.client-service'
 
 httpStatusSuccess = 200
